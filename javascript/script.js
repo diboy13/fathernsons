b660f50d4c0bf5db9a5c8ce599746e61151506c5
diboy13 <hadidicky31@gmail.com>
2024-04-18 03:55:09 +0700

    tambahan fitur

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ window.onscroll = function () {
   const fixedNav = header.offsetTop;
   const toTop = document.querySelector("#to-top");
 
-  if (window.pageYOffset > fixedNav) {
+  if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
     toTop.classList.remove("hidden");
     toTop.classList.add("flex");
@@ -18,6 +18,9 @@ window.onscroll = function () {
 // hamburger
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
+const darkTog = document.querySelector("#dark-tog");
+const darkTog1 = document.querySelector("#dark-tog1");
+const darkToggle = document.querySelector("#dark-toggle");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
@@ -27,8 +30,48 @@ hamburger.addEventListener("click", function () {
 // klik di luar hamburger
 
 window.addEventListener("click", function (e) {
-  if (e.target != hamburger && e.target != navMenu) {
+  if (
+    e.target != hamburger &&
+    e.target != navMenu &&
+    e.target != darkTog &&
+    e.target != darkTog1 &&
+    e.target != darkToggle
+  ) {
     hamburger.classList.remove("hamburger-active");
     navMenu.classList.add("hidden");
   }
 });
+
+// darkmode toggle
+const html = document.querySelector("html");
+const dark = document.querySelector("#dark");
+const light = document.querySelector("#light");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    dark.classList.toggle("hidden");
+    light.classList.toggle("hidden");
+    localStorage.setItem("theme", "dark");
+  } else {
+    html.classList.remove("dark");
+    dark.classList.toggle("hidden");
+    light.classList.toggle("hidden");
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// pindahkan posisi toggle sesuai tema
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+  dark.classList.toggle("hidden");
+  light.classList.toggle("hidden");
+} else {
+  darkToggle.checked = false;
+  dark.classList.toggle("hidden");
+  light.classList.toggle("hidden");
+}
